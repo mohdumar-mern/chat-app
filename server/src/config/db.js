0ebf34865c2fc5a5a8244fc05f069fcd4151e5d3
Mohd88umar <214827861+mohdumar-mern-dev@users.noreply.github.com>
2025-11-12 19:16:27 +0530

    Inital commit

--- server/src/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
-};
-
-export default connectDB;
