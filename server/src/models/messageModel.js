1502abd317bb635ebdacafcb560ff844acc768a7
Mohd88umar <214827861+mohdumar-mern-dev@users.noreply.github.com>
2025-11-13 12:53:31 +0530

    Added to conversation and message model

---
+++ server/src/models/messageModel.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const MessageSchema = new mongoose.Schema({
+  conversation: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation', required: true },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  text: { type: String, default: '' },
+  attachments: [{ type: String }],
+  readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  deliveredTo: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: true });
+
+const Message = mongoose.model('Message', MessageSchema);
+export default Message;
